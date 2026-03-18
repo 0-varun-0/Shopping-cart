2791a6e39a777752b1552832b5a7b53707cdde61
0-varun-0 <0xvarun4@gmail.com>
2026-03-18 11:38:52 +0530

    added middleware for exeption handling

--- obj/Debug/net10.0/ShoppingCart.AssemblyInfo.cs
+++ obj/Debug/net10.0/ShoppingCart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3494fa1197432687f90212e0a53e101395c3f67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
